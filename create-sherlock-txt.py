7223fe20ee0aff220ba846ccf2998ffcfda2cd0c
Rob Simmons <robs@brilliant.org>
2019-05-09 11:55:04 -0400

    Add create-sherlock-txt.py, which produces a single sherlock.txt from the directories

---
+++ create-sherlock-txt.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# coding=utf-8
+
+import os
+
+"""
+sed -i~ 's/[“”]/"/g' * && rm *~
+sed -i~ "s/[’‘]/'/g" * && rm *~
+"""
+
+out = open('sherlock.txt', 'w+')
+
+for file in sorted(os.listdir('stories')):
+  if file.endswith('.txt'):
+    with open('stories/' + file) as f:
+      out.write('\n' + '#'*80 + '\n')
+      out.write(f.readline())
+      out.write('\n' + '#'*80 + '\n')
+      for line in f.readlines():
+        out.write(line)
+
+for file in sorted(os.listdir('novels')):
+  if file.endswith('.txt'):
+    with open('novels/' + file) as f:
+      title = f.readline().strip()
+      part = ''
+      for line in f.readlines():
+        if line.startswith('PART'):
+          part = ', Part ' + line[5]
+        elif line.startswith('Chapter'):
+          chapterline = line.split('--')
+          chapter = chapterline[1].strip()
+          chapternum = chapterline[0].split(' ')[1]
+
+          fullname = title + part + ', Chapter ' + chapternum + ': ' + chapter
+          print(fullname)
+          out.write('\n' + '#'*80 + '\n')
+          out.write(fullname)
+          out.write('\n' + '#'*80 + '\n')
+        else:
+          out.write(line)
